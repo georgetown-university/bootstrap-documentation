a460edb18ba2910d40f11001b830e5354f203532
Joni Halabi <jh1955@georgetown.edu>
2017-03-08 09:45:11 -0500

    Code reorganization to get documentation into the root directory

--- _plugins/bridge.rb
+++ _plugins/bridge.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module Bridge
   class Generator < Jekyll::Generator
     def generate(site)
-      path = File.join(site.source, "../grunt/configBridge.json")
+      path = File.join(site.source, "grunt/configBridge.json")
       site.data["configBridge"] = YAML.load_file(path)
     end
   end
